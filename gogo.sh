5eb5e4e90ce60c7e154cef31159600510a416fd4
Martin <martin.runelov@gmail.com>
2013-05-19 13:22:30 +0200

    Added a comment about the wildcard used

--- gogo.sh
+++ gogo.sh
@@ -5,6 +5,10 @@
 # gogo
 # 
 # . gogo (to move to the specified directory in the current environment)
+#
+#
+# Uses the "..." wildcard to go into all subdirectories as well.
+#
 
 cd $HOME/Programming/mygo/src
 go fmt ../...
